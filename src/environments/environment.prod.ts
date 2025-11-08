877eb9a8ceb654d2b8d134b9882a833c611ae627
LuisRamos <killuaryu1118@gmail.com>
2025-11-08 13:00:52 -0500

    ruta para login en vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-control-inventario-desplegar.onrender.com/api',
-  wsUrl: 'https://backend-control-inventario-desplegar.onrender.com/ws'
+  wsUrl: 'wss://backend-control-inventario-desplegar.onrender.com/ws'
 };
