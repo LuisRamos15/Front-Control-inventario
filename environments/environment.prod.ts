239fd286b51fd9234adee3435fb2d85c280faa2b
LuisRamos <killuaryu1118@gmail.com>
2025-11-08 00:13:23 -0500

    chore(vercel): build prod + vercel.json + engines

---
+++ environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://<tu-backend-aws>/api',  // ajustar con URL real
+  wsUrl: 'wss://<tu-backend-aws>/ws',     // ajustar con URL real
+  topicProductos: '/topic/productos'
+};
